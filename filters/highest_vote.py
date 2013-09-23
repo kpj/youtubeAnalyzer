f3f0dfac51c25d90de73bf2d6542c2f0d580cc4b
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-09-23 16:12:41 +0200

    Improved "highest_vote"-filter

--- filters/highest_vote.py
+++ filters/highest_vote.py
@@ -1,9 +1,2 @@
 def apply(comments, args):
-	max_com = None
-	for com in comments:
-		if max_com == None:
-			max_com = com
-			continue
-		if com["upvotes"] - com["downvotes"] > max_com["upvotes"] - max_com["downvotes"]:
-			max_com = com
-	return max_com
+	return max(comments, key = lambda x: x['upvotes'] - x['downvotes'])
