715d988fea1ebd5428808d93c65cd1223f61b3ec
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-12-06 13:36:30 +0100

    Changed indentation

--- youtubeCommentAnalyzer.py
+++ youtubeCommentAnalyzer.py
@@ -10,59 +10,59 @@ import filters
 
 
 class YoutubeVideo(object):
-	def __init__(self, url):
-		self.id = re.findall(r'www.youtube.com/watch\?v=(.*)', url)[0]
+    def __init__(self, url):
+        self.id = re.findall(r'www.youtube.com/watch\?v=(.*)', url)[0]
 
-		self.comments = None
+        self.comments = None
 
-	def get_comments(self, page_num=1):
-		if self.comments == None:
-			self.comments = []
+    def get_comments(self, page_num=1):
+        if self.comments == None:
+            self.comments = []
 
-			for p in range(1, page_num + 1):
-				com_data_url = "http://www.youtube.com/all_comments?v=%s&page=%i" % (self.id, p)
-				soup = BeautifulSoup(urllib.urlopen(com_data_url))
-				for dat in soup.findAll("div", {"class": "content clearfix"}):
-					cur_com = {}
+            for p in range(1, page_num + 1):
+                com_data_url = "http://www.youtube.com/all_comments?v=%s&page=%i" % (self.id, p)
+                soup = BeautifulSoup(urllib.urlopen(com_data_url))
+                for dat in soup.findAll("div", {"class": "content clearfix"}):
+                    cur_com = {}
 
-					# check if comment was deleted
-					if dat.find("p").find("em") == None:
-						# find text
-						text = ""
-						container = dat.find("div", {"class": "comment-text"})
-						if container.find("p") != None:
-							text = container.find("p").text
-						else:
-							text = container.text
+                    # check if comment was deleted
+                    if dat.find("p").find("em") == None:
+                        # find text
+                        text = ""
+                        container = dat.find("div", {"class": "comment-text"})
+                        if container.find("p") != None:
+                            text = container.find("p").text
+                        else:
+                            text = container.text
 
-						# find author
-						meta = dat.find("p", {"class": "metadata"})
-						author = meta.find("span", {"class": "author"}).find("a").text
+                        # find author
+                        meta = dat.find("p", {"class": "metadata"})
+                        author = meta.find("span", {"class": "author"}).find("a").text
 
-						# find additional info
-						action = dat.find("div", {"class": "comment-actions"})
-						upvotes_data = action.find("span", {"class": "comments-rating-positive"})
-						upvotes = 0
-						downvotes = 0
-						if upvotes_data != None:
-							m = re.match(r'\D*?(\d+)\D*?(\d+)\D*?', upvotes_data.get("title"))
-							upvotes = int(m.groups()[0])
-							downvotes = int(m.groups()[1])
+                        # find additional info
+                        action = dat.find("div", {"class": "comment-actions"})
+                        upvotes_data = action.find("span", {"class": "comments-rating-positive"})
+                        upvotes = 0
+                        downvotes = 0
+                        if upvotes_data != None:
+                            m = re.match(r'\D*?(\d+)\D*?(\d+)\D*?', upvotes_data.get("title"))
+                            upvotes = int(m.groups()[0])
+                            downvotes = int(m.groups()[1])
 
-						# throw it all together
-						cur_com["text"] = text
-						cur_com["author"] = author
-						cur_com["upvotes"] = upvotes
-						cur_com["downvotes"] = downvotes
+                        # throw it all together
+                        cur_com["text"] = text
+                        cur_com["author"] = author
+                        cur_com["upvotes"] = upvotes
+                        cur_com["downvotes"] = downvotes
 
-						self.comments.append(cur_com)
+                        self.comments.append(cur_com)
 
-		return self.comments
+        return self.comments
 
-	def applyFilter(self, filter, *args):
-		if self.comments == None:
-			self.get_comments()
-		return filter.apply(self.comments, *args)
+    def applyFilter(self, filter, *args):
+        if self.comments == None:
+            self.get_comments()
+        return filter.apply(self.comments, *args)
 
 
 vid = YoutubeVideo(sys.argv[1])
